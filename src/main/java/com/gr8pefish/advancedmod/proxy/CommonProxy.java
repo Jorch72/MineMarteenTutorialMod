7082b5f7085298dc456264e70d9d4ed465b59ca8
Max <xwason@gmail.com>
2015-03-06 13:13:24 -0700

    Initial commit

---
+++ src/main/java/com/gr8pefish/advancedmod/proxy/CommonProxy.java
@@ -0,0 +1,13 @@
+package com.gr8pefish.advancedmod.proxy;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+public abstract class CommonProxy{
+    public abstract void preInit();
+
+    public abstract void init();
+
+    public abstract void postInit();
+
+    public abstract EntityPlayer getClientPlayer();
+}
---
+++ src/main/java/com/gr8pefish/advancedmod/proxy/ServerProxy.java
@@ -0,0 +1,27 @@
+package com.gr8pefish.advancedmod.proxy;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+public class ServerProxy extends CommonProxy{
+
+    @Override
+    public void preInit(){
+
+    }
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void postInit(){
+
+    }
+
+    @Override
+    public EntityPlayer getClientPlayer(){
+        return null;
+    }
+
+}
